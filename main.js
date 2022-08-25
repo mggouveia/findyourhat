e6413fdba509686add80b065203a388f32b7c1fd
Miguel <mggouveia@gmail.com>
2022-08-25 12:56:29 +0100

    principal commit

--- main.js
+++ main.js
@@ -25,13 +25,88 @@ class Field {
                 console.log('Out of bounds instruction!');
                 playing = false;
                 break;
-            } elseif (this.isHole()) {
+            } else if (this.isHole()) {
                 console.log('Sorry, you fell down a hole!');
                 playing = false;
                 break;
-            } elseif (this.isHat()) {
+            } else if (this.isHat()) {
                 console.log('Congrats, you found your lost hat!');
+                playing = false;
+                break;
             }
+            //Update current location on map
+            this.field[this.locationY][this.locationX] = pathCharacter;
         }
     }
+
+    askQuestion(){
+        const answer = prompt('Which way? ').to UpperCase();
+        switch(answer) {
+            case 'U':
+                this.locationY -= 1;
+                break;
+            case 'D':
+                this.locationY += 1;
+                break;
+            case 'L':
+                this.locationX -= 1;
+                break;
+            case 'R':
+                this.locationX += 1;
+                break;
+            default:
+                console.log('Enter U, D, L or R.');
+                this.askQuestion();
+                break;
+        }
+    }
+
+    isInBounds() {
+        return (
+            this.locationY >= 0 && 
+            this.locationX >= 0 &&
+            this.locationY < this.field.length &&
+            this.locationX < this.field[0].length
+        );
+    }
+
+    isHat(){
+        return this.field[this.locationY][this.locationX] === hat;
+    }
+
+    isHole(){
+        return this.field[this.locationY][this.locationX] === hole;
+    }
+
+    print(){
+        const displayString = this.field.map(row => {
+            return row.join('');
+        }).join ('\n');
+        console.log(displayString);
+    }
+
+    static generateField(height, width, percentage = 0.1) {
+        const field = new Array(height).fill(0).map(el => newArray(width));
+        for (let y = 0; y < height; y++){
+            for (let x = 0; x < width; x++) {
+                const prob = Math.random();
+                field[y][x] = prob > percentage ? fieldCharacter : hole;
+            }
+        }
+        //Set Hat Location
+        const hatLocation = {
+            x: Math.floor(Math.random() * width),
+            y: Math.floor(Math.random() * height)
+        };
+        //Making sure that "hat" is not at starting point
+        while (hatLocation.x === 0 &&& hatLocation.y === 0) {
+            hatLocation.x = Math.floor(Math.random()* width);
+            hatLocation.y = Math.floor(Math.random()* heigth);
+        }
+        field[hatLocation.y][hatLocation.x] = hat;
+        return field;
+    }
 }
+
+const myfield = new Field(Field.generateField(10, 10, 0.2));
+myfield.runGame();
